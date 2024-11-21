e41bc068c402d45cf3451750ace1fe16b1654c30
Egin Pardewan <eginpardewan@gmail.com>
2024-11-21 19:58:37 +0700

    Menambahkan class data Mahasiswa

---
+++ app/src/main/java/com/example/activity6/model/Mahasiswa.kt
@@ -0,0 +1,11 @@
+package com.example.activity6.model
+
+data class Mahasiswa(
+    val nama : String = " ",
+    val NIM : String = " ",
+    val gender : String = " ",
+    val noHp : String = " ",
+    val email : String = " ",
+    val alamat : String = " "
+
+)
